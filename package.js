d7b50a9690ea5524e9e364db37cb35d1afded18c
Ian Serlin <ian@ianserlin.com>
2013-11-06 00:16:58 -0800

    updating for meteor 0.6.5+

--- package.js
+++ package.js
@@ -1,13 +1,16 @@
 Package.describe({
-	summary: 'Balanced Payments (nbalanced packaged for meteor)'
+    summary: 'Balanced Payments (nbalanced packaged for meteor)'
 });
 
 Npm.depends({
-	'nbalanced': 'https://github.com/ianserlin/nbalanced/tarball/05eb18cf3536e22b62f349d0520e5df23740dd5c'
+    'nbalanced': 'https://github.com/ianserlin/nbalanced/tarball/05eb18cf3536e22b62f349d0520e5df23740dd5c'
 });
 
 Package.on_use(function (api) {
-	api.use('sync-methods', 'server');
-	api.add_files('index.js', 'server');
-	api.add_files('balanced.js', 'client');
+    api.use('sync-methods', 'server');
+
+    api.add_files('index.js', 'server');
+    api.add_files('balanced.js', 'client');
+
+    api.export('nbalanced', 'server');
 });
